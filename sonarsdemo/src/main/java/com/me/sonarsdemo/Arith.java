c08ccf8797878d5cca1f7287cd751667104e1866
bvk <kalloorkar@gmail.com>
2021-12-14 10:30:30 +0530

    Updated Arith.java

---
+++ sonarsdemo/src/main/java/com/me/sonarsdemo/Arith.java
@@ -0,0 +1,15 @@
+package com.me.sonarsdemo;
+
+public class Arith {
+	public int add(int first, int second){
+		return first + second;
+	}
+	
+	public int sub(int first, int second){
+		return first - second;
+	}
+	
+	public int multiply(int first, int second){
+		return first * second;
+	}
+}
