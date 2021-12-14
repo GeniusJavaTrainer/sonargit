44d287a4fb28216b2597306ce20f72babef2f77f
bvk <kalloorkar@gmail.com>
2021-12-14 11:38:58 +0530

    test merge

--- sonarsdemo/src/main/java/com/me/sonarsdemo/Arith.java
+++ sonarsdemo/src/main/java/com/me/sonarsdemo/Arith.java
@@ -12,4 +12,10 @@ public class Arith {
 	public int multiply(int first, int second){
 		return first * second;
 	}
+	
+	
+	public int divide(int first, int second){
+		return first / second;
+	}
+
 }
